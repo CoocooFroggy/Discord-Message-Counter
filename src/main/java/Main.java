0f7bb6c0e6376ab2ba30c5edc3b951834da02946
CoocooFroggy <johnandklay@gmail.com>
2022-01-29 00:16:54 -0500

    Prepare for release

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
@@ -10,7 +11,9 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         String token = System.getenv("MESSAGE_COUNTER_TOKEN");
-        JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+        JDABuilder jdaBuilder = JDABuilder
+                .createDefault(token)
+                .setActivity(Activity.watching("users' messages."));
         try {
             jda = jdaBuilder.build();
         } catch (LoginException e) {
@@ -20,17 +23,25 @@ public class Main {
         }
         jda.awaitReady();
 
-        if (args.length > 1 && args[0].equals("slash"))
+        if (args.length > 1 && args[0].equals("slash")) {
             if (args[1].equals("debug")) {
                 Guild guild = jda.getGuildById("685606700929384489"); // Coocoo's Test Server
                 assert guild != null;
                 guild.upsertCommand("count", "Counts a user's messages in this guild.")
                         .addOption(OptionType.USER, "user", "Count the messages from this user.", true)
-                        .queue();
-            } else
+                        .complete();
+                System.out.println("Upserted slash commands to " + guild.getName() + ".");
+                System.exit(0);
+                return;
+            } else if (args[1].equals("global")) {
                 jda.upsertCommand("count", "Counts a user's messages in this guild.")
                         .addOption(OptionType.USER, "user", "Count the messages from this user.", true)
-                        .queue();
+                        .complete();
+                System.out.println("Upserted slash commands globally.");
+                System.exit(0);
+                return;
+            }
+        }
 
         jda.addEventListener(new Listener());
         System.out.println("Started Message Counter!");
